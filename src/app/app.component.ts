c8fa974595cedbc104d80eb5fd3517e17b5a25b0
bruno-a472 <brunoangelo2015@hotmail.com>
2024-12-05 11:58:47 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TesteComponent } from './teste/teste.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,  TesteComponent],
   templateUrl: './app.component.html',
+  // template: `
+
+  // `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { TesteComponent } from './teste/teste.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        redirectTo: '/app',
+        pathMatch: 'full'
+    },
+    {
+        path: 'app',
+        component: AppComponent,
+        title: 'começo'
+    },
+    {
+        path: 'app/teste',
+        component: TesteComponent,
+        title: 'teste'
+    }
+    
+
+];
